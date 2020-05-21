de3a583b25d0f36cf4137690ccc1aa8962547fcf
Bertrand Atemkeng <ba@marketing-factory.de>
2020-05-21 12:19:51 +0200

    trim-product-title

--- scraper/scraper.js
+++ scraper/scraper.js
@@ -13,7 +13,10 @@ async function scraperPruduct(url) {
     const [el2] = await page.$x('//*[@id="productTitle"]');
     const txt = await el2.getProperty('textContent');
     const rawTxt = await txt.jsonValue();
+    const product_title = rawTxt.trim();
 
+
+   // var matches = rawTxt.match(/[a-zA-Z]+/);
 
     const [el3] = await page.$x('//*[@id="priceblock_ourprice"]');
     const txt2 = await el3.getProperty('textContent');
@@ -25,11 +28,20 @@ async function scraperPruduct(url) {
 
 
 
-    console.log({imgUrl, rawTxt, price, shipping});
+
+
+   // console.log({imgUrl, product_title, price, shipping});
+   //  console.log({url, imgUrl, product_title, price});
+     console.log({product_title, price});
+
 
     browser.close();
 
 }
 
 scraperPruduct('https://www.amazon.de/dp/B01MYADZ65/ref=dp_cerb_1');
+
+scraperPruduct('https://www.amazon.de/gp/product/B01FDLD52A/ref=ox_sc_act_title_1?smid=APLJ5AXPSKRV0&psc=1');
+scraperPruduct('https://www.amazon.de/gp/product/B07JQZ9KC5/ref=ox_sc_saved_title_4?smid=A2HPDDVOO82QB3&psc=1');
+scraperPruduct('https://www.amazon.de/gp/product/B07S9TDM3T/ref=ox_sc_saved_title_7?smid=ARNP85SXIOHPU&psc=1');
 
